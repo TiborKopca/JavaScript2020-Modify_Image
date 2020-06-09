f4dbdc499f4b65795bf58b7f960bd879bda1285d
Tibor Kopca <tibor.kopca@gmail.com>
2020-06-09 11:15:04 +0200

    NEW PROJECT - RESIZE IMAGE ON EVENT. Added new working files-index.html, resize.js, estilos.css. Changed some non interesting spaces.

---
+++ js/resize.js
@@ -0,0 +1,9 @@
+/*
+Hay que crear una funcionalidad en Javascript, para cuando el usuario pulse encima de una imagen de la web, esta aumente de tamaño y
+ al volver a pulsar disminuya.
+* Solo es necesario que en la web vea una sola imagen. Me interesa ver el código de JS.
+
+2020 @ Tibor Kopca
+*/
+'use strict'
+
