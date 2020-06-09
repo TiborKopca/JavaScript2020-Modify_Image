33f26de6a26e575a0a8216060f1def2e95a0e4f4
Tibor Kopca <tibor.kopca@gmail.com>
2020-06-09 14:41:38 +0200

    JS code added

--- js/resize.js
+++ js/resize.js
@@ -7,3 +7,15 @@ Hay que crear una funcionalidad en Javascript, para cuando el usuario pulse enci
 */
 'use strict'
 
+var img = document.getElementById('img1')
+img.addEventListener('mouseover', function(){
+    // img.style.width = '70vh';
+    // img.style.marginTop = '-30px';
+    img.style.transform = 'scale(1.5)';
+    img.style.transition = 'all 1s linear';
+})
+img.addEventListener('mouseout', function(){
+    // img.style.width = '50vh';
+    // img.style.marginTop = '0px';
+    img.style.transform = 'scale(1)';
+})
